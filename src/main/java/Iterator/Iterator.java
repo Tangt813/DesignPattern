e1c7a9e29512d36aed9340825d940b275ab7cc74
tts <823206295@qq.com>
2021-10-22 13:41:57 +0800

    Iterator

---
+++ src/main/java/Iterator/Iterator.java
@@ -0,0 +1,15 @@
+package Iterator;
+
+/**
+ * @author tangshuo
+ * @version 1.0.0
+ * @ClassName Iterator.java
+ * @Description 迭代器接口
+ * @createTime 2021年10月17日 12:58:00
+ */
+public interface Iterator {
+
+    public abstract boolean hasNext();
+
+    public abstract Object next();
+}
