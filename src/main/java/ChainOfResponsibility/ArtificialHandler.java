3da54d455b61f14b7b48bb1ee09e15eecc082dcc
ZhichenRen <1337795211@qq.com>
2021-10-26 14:08:02 +0800

    update chain of responsibility

--- src/main/java/ChainOfResponsibility/ArtificialHandler.java
+++ src/main/java/ChainOfResponsibility/ArtificialHandler.java
@@ -15,12 +15,12 @@ public class ArtificialHandler extends Handler{
     public Boolean handle(RefundRequest request) {
         LocalDate today = LocalDate.now();
         LocalDate buyDate = LocalDate.ofInstant(request.getBuyDate().toInstant(), ZoneId.systemDefault());
-        Integer duration = Period.between(buyDate, today).getDays();
-        if(request.getSource().equals("Official") && duration <= 30){
+        Long duration = today.toEpochDay() - buyDate.toEpochDay();
+        if(duration <= 60){
             System.out.println("人工审核通过，退票成功！");
             return Boolean.TRUE;
         }
-        System.out.println("您的条件不满足退票要求，退票失败。");
+        System.out.println("您的购票时间已经超过了60天，不满足退票要求，退票失败。");
         return Boolean.FALSE;
     }
 }
--- src/test/java/ChainOfResponsibility/ChainOfResponsibilityTest.java
+++ src/test/java/ChainOfResponsibility/ChainOfResponsibilityTest.java
@@ -1,11 +1,15 @@
 package ChainOfResponsibility;
 
+import Filter.Ticket;
 import Template.BasePerformance;
 import Template.DolphinPerformance;
 import Template.SeaLionPerformance;
 import Template.SealPerformance;
 import org.junit.Test;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -19,11 +23,34 @@ import java.util.List;
  */
 public class ChainOfResponsibilityTest {
     @Test
-    public void performTest(){
+    public void performTest() throws ParseException {
         System.out.println("--------------------Chain of Responsibility Pattern Test Start--------------------");
-        RefundRequest request = new RefundRequest(new Date(2021, Calendar.OCTOBER, 1), "Official", 500);
         Handler autoHandler = new AutoHandler();
-        System.out.println(autoHandler.handle(request));
+        System.out.println("一名游客准备办理退票，他的购票信息如下。");
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Ticket ticket1 = new Ticket("Tom", 100, dateFormat.parse("2021-08-01"), "Adult");
+        System.out.println("Ticket : [ Name : " + ticket1.getUserName() +
+                ", Price : " + ticket1.getPrice() +
+                ", Date : " + ticket1.getTime() +
+                ", Type : " + ticket1.getType() + " ] ");
+        RefundRequest request1 = new RefundRequest(ticket1);
+        autoHandler.handle(request1);
+        System.out.println("另一名游客也准备办理退票，他的购票信息如下。");
+        Ticket ticket2 = new Ticket("Tooom", 100, dateFormat.parse("2021-10-01"), "Adult");
+        System.out.println("Ticket : [ Name : " + ticket2.getUserName() +
+                ", Price : " + ticket2.getPrice() +
+                ", Date : " + ticket2.getTime() +
+                ", Type : " + ticket2.getType() + " ] ");
+        RefundRequest request2 = new RefundRequest(ticket2);
+        autoHandler.handle(request2);
+        System.out.println("有一名游客刚买完票发现买错了类型，准备退票，他的购票信息如下。");
+        Ticket ticket3 = new Ticket("Tommmm", 100, dateFormat.parse("2021-10-26"), "Adult");
+        System.out.println("Ticket : [ Name : " + ticket3.getUserName() +
+                ", Price : " + ticket3.getPrice() +
+                ", Date : " + ticket3.getTime() +
+                ", Type : " + ticket3.getType() + " ] ");
+        RefundRequest request3 = new RefundRequest(ticket3);
+        autoHandler.handle(request3);
         System.out.println("--------------------Chain of Responsibility Pattern Test End--------------------");
     }
 }
