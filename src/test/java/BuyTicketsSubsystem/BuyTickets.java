6bd2158b16236934e5b59d0a2ff9864cd83f3bb7
ZhichenRen <1337795211@qq.com>
2021-10-31 22:15:23 +0800

    update ticket scene

--- src/test/java/BuyTicketsSubsystem/BuyTickets.java
+++ src/test/java/BuyTicketsSubsystem/BuyTickets.java
@@ -1,14 +1,21 @@
 package BuyTicketsSubsystem;
 
+import ChainOfResponsibility.AutoHandler;
+import ChainOfResponsibility.BaseHandler;
+import ChainOfResponsibility.RefundRequest;
 import Decorated.Customer;
 import Decorated.People;
 import Decorated.VIPCustomer;
+import Filter.Ticket;
 import Singleton.SerialNumberObject;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Scanner;
 
 public class BuyTickets {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         System.out.println("欢迎来到线上购票系统");
         Scanner scanner = new Scanner(System.in);
         System.out.println("1.购票\n2.退票\n3.退出");
@@ -45,7 +52,37 @@ public class BuyTickets {
             }
             else if(command.equals("2")){
                 //在这里写退票
-                System.out.println("退票");
+                System.out.println("欢迎来到退票系统，下面进入自动退票业务办理");
+                System.out.println("请输入你的姓名：");
+                String name;
+                name = scanner.nextLine();
+                System.out.println("请输入你的ID：");
+                String id;
+                id = scanner.nextLine();
+                People user = new Customer(id);
+                if (id.charAt(0) == '1') {
+                    user = new VIPCustomer(user);
+                }
+                String info = user.buyTicket();
+                double price = 200;
+                if (info.equals("discount")) {
+                    price = price * 0.8;
+                }
+                else if (info.equals("free")) {
+                    price = 0;
+                }
+                System.out.println("请输入您的购票日期（yyyy-MM-dd）：");
+                String dateString = scanner.nextLine();
+                DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                Ticket ticket = new Ticket(name, (int)price, dateFormat.parse(dateString), info);
+                System.out.println("您的购票信息如下：");
+                System.out.println("Ticket : [ Name : " + ticket.getUserName() +
+                        ", Price : " + ticket.getPrice() +
+                        ", Date : " + ticket.getTime() +
+                        ", Type : " + ticket.getType() + " ] ");
+                BaseHandler autoHandler = new AutoHandler();
+                RefundRequest request1 = new RefundRequest(ticket);
+                autoHandler.handle(request1);
             }
             else{
                 continue;
