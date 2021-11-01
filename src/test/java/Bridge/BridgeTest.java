19269ddaae1a2a83b8252770fdd7c4563a06356e
tts <823206295@qq.com>
2021-11-01 19:20:24 +0800

    BusinessDelegateUpdates

--- src/test/java/Bridge/BridgeTest.java
+++ src/test/java/Bridge/BridgeTest.java
@@ -16,12 +16,14 @@ public class BridgeTest {
         System.out.println("桥接模式");
         System.out.println("在现实生活中，某些类具有两个或多个维度的变化，" +
                 "如果用继承方式，不但对应的子类很多，而且扩展困难。\n选择桥接模式，将抽象与实现分离，" +
-                "使它们可以独立变化。\n它是用组合关系代替继承关系来实现，从而降低了抽象和实现这两个可变维度的耦合度。\n");
+                "使它们可以独立变化。\n它是用组合关系代替继承关系来实现，从而降低了抽象和实现这两个可变维度的耦合度。");
+        System.out.println("------------------------------------------------------------------");
         System.out.println("接下来我们通过游乐设施来感受桥接模式的使用:");
         System.out.println("我们知道，同样种类的游乐设施可能会有不同的适合人群。\n而若是每一种游乐设施都同要与其适合人群叠在一起构造子类" +
                 "将会需要开发非常多的子类，而且拓展困难。\n而若使用桥接模式，将抽象与实现分离，" +
                 "使它们可以独立变化。\n它是用组合关系代替继承关系来实现，从而降低了抽象和实现这两个可变维度的耦合度。\n" +
-                "接下来将通过儿童与青年这两类不同的人群同游乐设施组合的方式来体现桥接模式\n");
+                "接下来将通过儿童与青年这两类不同的人群同游乐设施组合的方式来体现桥接模式");
+        System.out.println("------------------------------------------------------------------");
 
 
         System.out.println("我们来新建一个儿童对象，名字为小明，不是我们嘉年华的VIP");
@@ -31,8 +33,10 @@ public class BridgeTest {
         System.out.println("我们来新建一个青年对象，名字为张伟，是我们嘉年华的VIP");
         Youth youth = new Youth("张伟",true);
         System.out.println(youth.getName() + "是" + youth.getAgeGroupName() );
+        System.out.println("------------------------------------------------------------------");
 
         System.out.println("通过游乐设施与其适合人群的组合构造出不同的游乐设施");
+        System.out.println("------------------------------------------------------------------");
         BigHammer bigHammer4Child = new BigHammer(child);
         BigHammer bigHammer4Youth = new BigHammer(youth);
 
@@ -44,12 +48,18 @@ public class BridgeTest {
 
 
         child.take(bigHammer4Child);
+        System.out.println("------------------------------------------------------------------");
         child.take(bigHammer4Youth);
+        System.out.println("------------------------------------------------------------------");
 
         youth.take(rollerCoaster4Child);
+        System.out.println("------------------------------------------------------------------");
         youth.take(rollerCoaster4Youth);
+        System.out.println("------------------------------------------------------------------");
 
         child.take(carousel4Child);
+        System.out.println("------------------------------------------------------------------");
         child.take(carousel4Youth);
+        System.out.println("------------------------------------------------------------------");
     }
 }
--- src/test/java/BusinessDelegate/BusinessDelegateTest.java
+++ src/test/java/BusinessDelegate/BusinessDelegateTest.java
@@ -13,12 +13,14 @@ public class BusinessDelegateTest {
     @Test
     public void businessDelegateTest() {
         System.out.println("业务代表模式");
-        System.out.println("用于对表示层和业务层解耦。它基本上是用来减少通信或对表示层代码中的业务层代码的远程查询功能。\n");
+        System.out.println("用于对表示层和业务层解耦。它基本上是用来减少通信或对表示层代码中的业务层代码的远程查询功能。");
+        System.out.println("------------------------------------------------------------------");
         System.out.println("接下来我们通过纪念品商店的仓库来感受桥接模式的使用:");
         System.out.println("我们设计纪念品商店的智能仓库，员工不需要亲自进入仓库取货、存货，只需要通过一个仓库智能终端即可完成工作。\n" +
                 "仓库提供多种服务，如取货服务、存货服务。\n" +
                 "还设计来无法找到服务来保障程序的鲁棒性需求" +
-                "实现表示层与业务层的解耦\n");
+                "实现表示层与业务层的解耦");
+        System.out.println("------------------------------------------------------------------");
 
         System.out.println("我们首先建立一个空仓库");
         Warehouse warehouse = new Warehouse();
@@ -27,21 +29,23 @@ public class BusinessDelegateTest {
         WarehouseDelegate warehouseDelegate = new WarehouseDelegate();
 
         WarehouseClient client = new WarehouseClient(warehouseDelegate);
-
-        System.out.println("\n启用查找货物服务");
+        System.out.println("------------------------------------------------------------------");
+        System.out.println("启用查找货物服务");
         warehouseDelegate.setServiceType(WarehouseServiceType.PickUp);
         client.doTask(1,"red", warehouse);
         System.out.println("可以发现并未找到这件货物");
-
-        System.out.println("\n启用存储货物服务");
+        System.out.println("------------------------------------------------------------------");
+        System.out.println("启用存储货物服务");
         warehouseDelegate.setServiceType(WarehouseServiceType.Storage);
         client.doTask(1,"red", warehouse);
 
-        System.out.println("\n启用查找货物服务");
+        System.out.println("------------------------------------------------------------------");
+        System.out.println("启用查找货物服务");
         warehouseDelegate.setServiceType(WarehouseServiceType.PickUp);
         client.doTask(1,"red", warehouse);
 
-        System.out.println("\n测试未找到服务");
+        System.out.println("------------------------------------------------------------------");
+        System.out.println("测试未找到服务");
         warehouseDelegate.setServiceType(WarehouseServiceType.NotFound);
         client.doTask();
     }
