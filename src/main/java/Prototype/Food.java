e1722aa5bd2f082a138bd508e0cb86897b47d052
Yiyao Li <1256495914@qq.com>
2021-11-02 10:58:04 +0800

    lyy-11.2

--- src/main/java/Prototype/Food.java
+++ src/main/java/Prototype/Food.java
@@ -7,26 +7,18 @@ public abstract class Food implements Cloneable {
     protected double price;
 
 
-
-
     public double getPrice(){
         return price;
     }
-
     public String getName(){ return name;}
-
     public String getId() {
         return id;
     }
-
     public void setId(String id) {
         this.id = id;
     }
-
     public void setName(String name){ this.name=name;}
-
     public void setPrice(double price){ this.price=price;}
-
 
     public Object clone() {
         Object clone = null;
