be5f7e194c8428c65cc07d14f2135fb622030ff4
Yiyao Li <1256495914@qq.com>
2021-11-04 22:08:35 +0800

    lyy-11.4

--- src/test/java/Command/CommandTest.java
+++ src/test/java/Command/CommandTest.java
@@ -5,8 +5,9 @@ public class CommandTest {
     @Test
     public void commandTest() {
         System.out.println("#命令模式(Command Pattern)将一个请求封装成一个对象，从而可以用不同的请求对目标进行参数化");
-        System.out.println("#这种模式中提供一个命令的接口并实现具体的命令类以及作为调用对象的类");
-        System.out.println("#这种模式中创建一个作为代用对象的类，它会根据命令的类型确定哪个对象执行哪个命令");
+        System.out.println("#这种模式中提供一个命令的接口并实现具体的命令类以及作为调用对象的类，它会根据命令的类型确定哪个对象执行哪个命令");
+        System.out.println("#软件系统中行为请求者与行为实现者通常是一种紧耦合的关系，但某些时候，比如需要对行为进行记录、撤销或重做、事务等处理时，这种紧耦合的设计就显得无法很好地面对变化");
+        System.out.println("#以纪念品商店为例，商店中商品的进货和出售命令很可能被撤销或重做，并且我们需要确保这些命令都可以被记录和被追溯，此时命令模式就可以很好地解决这一点");
         System.out.println("#命令模式属于行为型模式");
         System.out.println("----------------------Command Pattern Test Start--------------------");
         System.out.println("纪念品商店要执行对水晶球的进货和出售任务");
