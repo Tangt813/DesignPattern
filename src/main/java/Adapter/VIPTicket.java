d8da4f3475add4055ec59bbff1dc8d634a6a58f5
tangt813201 <78403006+Tangt813@users.noreply.github.com>
2021-11-04 23:35:39 +0800

    谢宇翔修改

--- src/main/java/Adapter/VIPTicket.java
+++ src/main/java/Adapter/VIPTicket.java
@@ -9,7 +9,7 @@ public class VIPTicket implements AdvancedTicket{
     {
     }
     @Override
-    public String checkVIP(String TicketType)
+    public String checkVIP()
     {
         return "您为尊贵的VIP用户，凭VIP票即可游玩所有";
     }
--- src/test/java/Adapter/AdapterTest.java
+++ src/test/java/Adapter/AdapterTest.java
@@ -3,7 +3,7 @@ package Adapter;
 import org.junit.Test;
 
 /**
- * @author xieyuxiang
+ * @author XieYuxiang
  * @version 1.0.0
  * @ClassName AdapterTest.java
  * @Description TODO
@@ -13,10 +13,12 @@ public class AdapterTest
 {
     @Test
     public void adapterTest() {
+        System.out.println("适配器模式的作用是作为两个不兼容的接口之间的桥梁，将无法连接的2个物体可以交互起来");
+        System.out.println("如读卡器就是作为笔记本电脑接口和内存卡之间的桥梁");
         System.out.println("-------------------Test Adapter---------------------");
 
         try {
-            Thread.sleep(1000);
+            Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -24,24 +26,27 @@ public class AdapterTest
         System.out.println("我们拥有一个门票检测系统，能够检测门票的等级，但接口只能检测普通的基本的门票类型");
         System.out.println("在园区不断发展时，肯定会推出各种各样的票，那时候如果没有一个能够适配所有门票的接口，将会导致修改的工作量变大");
 
+
+
+        System.out.println("所以为了能够适配各种各样的票的类型，实现了适配器类，来让门票检验允许与所有票类型对接");
         try {
-            Thread.sleep(1000);
+            Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
-        System.out.println("所以为了能够适配各种各样的票的类型，实现了适配器类，来让门票检验允许与所有票类型对接");
         RegularTicket myTicket=new RegularTicket();
         System.out.println("-----------------------------------------------------------------");
         System.out.println("首先我们测试普通票：");
+        System.out.println("票的信息将设置为Regular");
         System.out.println(myTicket.check("Regular"));
         try {
-            Thread.sleep(1000);
+            Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         System.out.println("-----------------------------------------------------------------");
         System.out.println("下面我们测试VIP票：");
+        System.out.println("票的信息将设置为VIP");
         System.out.println(myTicket.check("VIP"));
 
     }
